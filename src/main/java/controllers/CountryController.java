3c737aa79c8eb77cca32339fcf3e5eb32ef480fb
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-31 21:19:50 -0300

    returning a Collection!

--- src/main/java/controllers/CountryController.java
+++ src/main/java/controllers/CountryController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,21 @@ import models.Country;
 @RestController
 public class CountryController {
 
+	
+	/*
+	 * By default Spring creates a String representation of the object and converts it to JSON!
+	 */
+	
 	@GetMapping("/france")
 	public Country france() {
 		Country c = Country.of("France", 67);
 		return c;
+	}
+	
+	@GetMapping("/all")
+	public List<Country> countries() {
+		Country c1 = Country.of("France", 67);
+		Country c2 = Country.of("Spain", 47);
+		return List.of(c1, c2);  // In Java 9 and later, List.of() is designed to return an instance of java.util.ImmutableCollections.ListN, which is an internal, highly optimized implementation of an immutable list.
 	}
 }
