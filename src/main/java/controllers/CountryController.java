c1e1d36aef8b96bacce2010fece8a4a5468c3641
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-31 21:31:54 -0300

    Using ResponseEntity!

--- src/main/java/controllers/CountryController.java
+++ src/main/java/controllers/CountryController.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,31 @@ public class CountryController {
 
 	
 	/*
-	 * By default Spring creates a String representation of the object and converts it to JSON!
+	 * By default Spring creates a String representation of the object/collection and converts it to JSON!
 	 */
-	
+	/*
 	@GetMapping("/france")
 	public Country france() {
 		Country c = Country.of("France", 67);
 		return c;
 	}
+	*/
+	
+	/*
+	 * Setting the response status and headers, with the body!
+	 */
+	@GetMapping("/france")
+	public ResponseEntity<Country> france() {
+		Country c = Country.of("France", 67);
+		return ResponseEntity
+				.status(HttpStatus.ACCEPTED)
+				.header("continent", "Europe")
+				.header("capital", "Paris")
+				.header("favorite_food", "cheese and wine")
+				.body(c);
+	}
+	
+	
 	
 	@GetMapping("/all")
 	public List<Country> countries() {
