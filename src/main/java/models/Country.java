fab708ea81ec943d6f778913bcfa029f5b6e8c00
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-31 21:09:01 -0300

    DATA TRANSFER MODEL Country

---
+++ src/main/java/models/Country.java
@@ -0,0 +1,38 @@
+package models;
+
+/*
+ * DATA TRANSFER MODEL (DTO)
+ */
+public class Country {
+
+	private String name;
+	private int population;
+	 
+	  
+    public static Country of(
+    		String name, 
+    		int population) {
+    	
+        Country country = new Country();
+        country.setName(name);
+        country.setPopulation(population);
+        return country;
+        
+    }
+    
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    public void setPopulation(int pop) {
+    	this.population = pop;
+    }
+    public String getName() {
+    	return this.name;
+    }
+    public int getPopulation() {
+    	return this.population;
+    }
+    
+	
+}
